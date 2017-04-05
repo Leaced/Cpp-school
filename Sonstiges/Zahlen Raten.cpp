2e07263e0682fdad73993c236aefdb9c098d05d9
Leaced <thokluba@web.de>
2017-04-05 13:32:42 +0200

    Umstrukturierung

--- Sonstiges/Zahlen Raten.cpp
+++ Sonstiges/Zahlen Raten.cpp
@@ -9,15 +9,14 @@ int main()
 {	
 	bool again = true;
 	
-	while(again==true)
+	while(again)
 	{
-		short randNumber = 0, tries = 0, number = 0;
+		srand(time(NULL));
+		again = false;
+		short randNumber = rand()%100+1, tries = 0, number = 0;
 		std::string input;
 		
 		std::cout << "Errate die richtige Zahl zwischen 1 und 100: ";
-		
-		srand(time(NULL));
-		randNumber = rand()%100+1;
 		
 		while(number!=randNumber)
 		{
@@ -25,24 +24,16 @@ int main()
 			getline(std::cin, input);
 			stringstream(input) >> number;
 			
-			if(number<randNumber)
-			{
-				std::cout << "\nDie eingegebene Zahl ist zu klein! Versuche es erneut: ";
-			}
-			if(number>randNumber)
-			{
-				std::cout << "\nDie eingegebene Zahl ist zu gro\xE1! Versuche es erneut: ";
-			}
+			if(number<randNumber) std::cout << "\nDie eingegebene Zahl ist zu klein! Versuche es erneut: ";		
+			else if(number>randNumber) std::cout << "\nDie eingegebene Zahl ist zu gro\xE1! Versuche es erneut: ";		
 		}
-		
-		number = 0;
 		
 		//Zusatzaufgabe: Berechne die minimal notwendige Anzahl an Versuchen
 		std::cout << "\nTreffer! Du hast " << tries << " Versuche gebraucht.\nWenn du ein 2. mal spielen m\x94 \bchtest, gib \"1\" ein. Zum beenden w\x84hle eine beliebige Eingabe: ";
 		getline(std::cin, input);
-		stringstream(input) >> randNumber;
-		std::cout << endl << endl;
-		if(randNumber == 1) again = randNumber;
+		stringstream(input) >> randNumber;	
+		if(randNumber == 1) again = true;
+		std::cout << "\n\n";
 	}
 	
 	return(0);
