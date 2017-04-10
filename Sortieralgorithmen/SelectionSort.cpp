edf26de378da1ba3ac05932eba83ffe2b8307250
Leaced <thokluba@web.de>
2017-04-10 14:57:05 +0200

    dynamischer Speicher

--- Sortieralgorithmen/SelectionSort.cpp
+++ Sortieralgorithmen/SelectionSort.cpp
@@ -5,6 +5,9 @@
 #include<cstdlib>
 #include<ctime>
 
+void SelectionSort(long double *array, unsigned short amount);
+void SelectionSort(int *array, unsigned short amount);
+
 int main()
 {
 	std::string input;
@@ -14,28 +17,35 @@ int main()
 	getline(std::cin, input);
 	std::stringstream(input) >> amount;
 	
-	long double array[amount];
-	
 	std::cout << "\nM\x94 \bchtest du mit zuf\x84lligen Zahlen arbeiten? y/n	";
 	getline(std::cin, input);
 	if(input == "y" || input == "Y")
 	{
+		int *array = new int[amount];
 		srand(time(0));
-		for(unsigned short idx = 0; idx < amount; idx++)
-		{
-			array[idx] = rand()%100;
-		}
+		for(unsigned short idx = 0; idx < amount; idx++) array[idx] = rand()%100;
+		SelectionSort(array, amount);
+		delete[] array;
 	}
 	else
 	{
+		long double *array = new long double[amount];
 		for(unsigned short idx = 0; idx < amount; idx++)
 		{
 			std::cout << "Gib die " << idx+1 << ". Zahl ein: ";
 			getline(std::cin,input);
 			std::stringstream(input) >> array[idx];
 		}
+		SelectionSort(array, amount);
+		delete[] array;
 	}
 	
+	getch();
+	return(0);
+}
+	
+void SelectionSort(long double *array, unsigned short amount)
+{
 	std::cout << "\n";
 	
 	for(unsigned short idx =0; idx<amount-1; idx++) std::cout << array[idx] << ", ";
@@ -56,7 +66,28 @@ int main()
 		for(unsigned short idx =0; idx<amount-1; idx++) std::cout << array[idx] << ", ";
 		std::cout << array[amount-1] << "\n"; //Ausgabe des Arrays nach jedem Sortierungsschritt
 	}
+}
+
+void SelectionSort(int *array, unsigned short amount)
+{
+	std::cout << "\n";
 	
-	getch();
-	return(0);
+	for(unsigned short idx =0; idx<amount-1; idx++) std::cout << array[idx] << ", ";
+	std::cout << array[amount-1] << "\n"; //Ausgabe des unsortierten Arrays
+	
+	for(unsigned short idx = 0;  idx<amount-1; idx++)
+	{
+		unsigned short indexMin = idx;
+		for(unsigned short idx2 = idx+1; idx2<amount; idx2++) if(array[idx2]<array[indexMin]) indexMin = idx2;
+		
+		if(indexMin!=idx)
+		{
+			long double temp = array[idx];
+			array[idx] = array[indexMin];
+			array[indexMin] = temp;
+		}
+		
+		for(unsigned short idx =0; idx<amount-1; idx++) std::cout << array[idx] << ", ";
+		std::cout << array[amount-1] << "\n"; //Ausgabe des Arrays nach jedem Sortierungsschritt
+	}
 }
